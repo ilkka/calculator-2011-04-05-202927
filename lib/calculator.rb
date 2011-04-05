e3318e918d1a2e2cd412ce2bea74271661a4654f
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-04-05 22:11:39 +0300

    Implement diff, refactored tokenization

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -9,16 +9,24 @@ class Calculator
   end
 
   def add
+    expr_to_numbers.inject(0) { |sum,n| sum += n }
+  end
+
+  def diff
+    nums = expr_to_numbers
+    nums[1..-1].inject(nums[0]) { |sum,n| sum -= n }
+  end
+
+  private
+
+  def expr_to_numbers
     nums = @expr.split(/[,\n]/).map { |part|
       raise IllFormedExpressionError, 'multiple consecutive delimiters not allowed' if part.empty?
       part.to_i
     }
     negatives = nums.select {|n| n < 0}
     raise IllFormedExpressionError, "negatives not allowed: #{negatives.join ','}" unless negatives.empty?
-    nums.inject(0) { |sum,n| sum += n }
+    return nums
   end
 
-  def diff
-    return 1
-  end
 end
